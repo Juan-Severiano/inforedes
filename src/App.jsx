6f3210048a20f52f0d19a6c230fbfe94cc3cfd28
juan <juansoussev@gmail.com>
2023-08-19 22:08:24 -0300

    Iniciando a página inicial

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,13 @@ import Header from './components/Header'
 export default function App() {
 
   return (
-    <Header />
+    <>
+      <Header />
+      <section className="container d-flex">
+        <aside className='container-half'>
+          <h1><p>consertar seu computador </p><strong>nunca</strong><p className="easy"><span>foi tão fácil</span></p></h1>
+        </aside>
+      </section>
+    </>
   )
 }
